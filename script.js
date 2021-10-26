d314259a9488d851c23e82e67b8ff8044be11f87
MakhovRoman <70774476+MakhovRoman@users.noreply.github.com>
2021-10-26 17:35:58 +0300

    Start

---
+++ script.js
@@ -0,0 +1,304 @@
+let json = {
+    "Sheet1": [
+        {
+            "name": "Канкулова Милана",
+            "gender": "Ж",
+            "age": "10",
+            "weight": "24.9",
+            "ku": "нет",
+            "city": "Кабардино-Балкарская Р.",
+            "coach": "Кармоков А.Х."
+        },
+        {
+            "name": "Мухаметзянова Регина",
+            "gender": "Ж",
+            "age": "10",
+            "weight": "30",
+            "ku": "8",
+            "city": "Михайловск",
+            "coach": "Якименко О.В."
+        },
+        {
+            "name": "Кривко Елизавета",
+            "gender": "Ж",
+            "age": "10",
+            "weight": "32.8",
+            "ku": "7",
+            "city": "Ставрополь",
+            "coach": "Кривко С.С."
+        },
+        {
+            "name": "Аксенова Валерия",
+            "gender": "Ж",
+            "age": "11",
+            "weight": "33.7",
+            "ku": "8",
+            "city": "Сальск",
+            "coach": "Зима С.С."
+        },
+        {
+            "name": "Гринь Ксения",
+            "gender": "Ж",
+            "age": "10",
+            "weight": "36",
+            "ku": "8",
+            "city": "Ставрополь",
+            "coach": "Орлов М.В."
+        },
+        {
+            "name": "Саутич Ярослав",
+            "gender": "М",
+            "age": "8",
+            "weight": "24.5",
+            "ku": "нет",
+            "city": "Кабардино-Балкарская Р.",
+            "coach": "Кармоков А.Х."
+        },
+        {
+            "name": "Мирзоев Абакайлав ",
+            "gender": "М",
+            "age": "10",
+            "weight": "25,5",
+            "ku": "8",
+            "city": "Р. Дагестан",
+            "coach": "Темирханов М.Б."
+        },
+        {
+            "name": "Кузнецов Максим",
+            "gender": "М",
+            "age": "9",
+            "weight": "26.5",
+            "ku": "нет",
+            "city": "Кабардино-Балкарская Р.",
+            "coach": "Кармоков А.Х."
+        },
+        {
+            "name": "Гриценко Кирилл",
+            "gender": "М",
+            "age": "10",
+            "weight": "27",
+            "ku": "8",
+            "city": "Ставрополь",
+            "coach": "Рябко О.Г."
+        },
+        {
+            "name": "Трепачко Богдан",
+            "gender": "М",
+            "age": "10",
+            "weight": "28",
+            "ku": "7",
+            "city": "Ставрополь",
+            "coach": "Махов Р.С."
+        },
+        {
+            "name": "Кузьменко Егор",
+            "gender": "М",
+            "age": "10",
+            "weight": "28",
+            "ku": "8",
+            "city": "Ставрополь",
+            "coach": "Рябко О.Г."
+        },
+        {
+            "name": "Сутримин Вадим",
+            "gender": "М",
+            "age": "10",
+            "weight": "29",
+            "ku": "7",
+            "city": "Ставрополь",
+            "coach": "Рябко О.Г."
+        },
+        {
+            "name": "Некрасов Александр",
+            "gender": "М",
+            "age": "10",
+            "weight": "29",
+            "ku": "9",
+            "city": "Ставрополь",
+            "coach": "Рябко О.Г."
+        },
+        {
+            "name": "Одижев Дамир",
+            "gender": "М",
+            "age": "8",
+            "weight": "29.5",
+            "ku": "нет",
+            "city": "Кабардино-Балкарская Р.",
+            "coach": "Кармоков А.Х."
+        },
+        {
+            "name": "Бижев Алан",
+            "gender": "М",
+            "age": "10",
+            "weight": "29.9",
+            "ku": "нет",
+            "city": "Кабардино-Балкарская Р.",
+            "coach": "Кармоков А.Х."
+        },
+        {
+            "name": "Шагидов Сулейман",
+            "gender": "М",
+            "age": "10",
+            "weight": "30",
+            "ku": "8",
+            "city": "Р. Дагестан",
+            "coach": "Темирханов М.Б."
+        },
+        {
+            "name": "Меселов Ахмад",
+            "gender": "М",
+            "age": "9",
+            "weight": "30",
+            "ku": "8",
+            "city": "Р. Дагестан",
+            "coach": "Темирханов М.Б."
+        },
+        {
+            "name": "Алиев Тажудин",
+            "gender": "М",
+            "age": "11",
+            "weight": "32",
+            "ku": "7",
+            "city": "Р. Дагестан",
+            "coach": "Омаров М.Ш."
+        },
+        {
+            "name": "Гужвинский Мирослав",
+            "gender": "М",
+            "age": "10",
+            "weight": "32",
+            "ku": "7",
+            "city": "Михайловск",
+            "coach": "Махов В.С."
+        },
+        {
+            "name": "Полупанов Константин",
+            "gender": "М",
+            "age": "10",
+            "weight": "32",
+            "ku": "8",
+            "city": "Михайловск",
+            "coach": "Якименко О.В."
+        },
+        {
+            "name": "Карманов Андрей",
+            "gender": "М",
+            "age": "11",
+            "weight": "33",
+            "ku": "8",
+            "city": "Михайловск",
+            "coach": "Швец А.П."
+        },
+        {
+            "name": "Троянов Андрей",
+            "gender": "М",
+            "age": "10",
+            "weight": "33",
+            "ku": "8",
+            "city": "Ставрополь",
+            "coach": "Айзатулин Т.Ш."
+        },
+        {
+            "name": "Бондарев Егор",
+            "gender": "М",
+            "age": "10",
+            "weight": "34",
+            "ku": "7",
+            "city": "Ставрополь",
+            "coach": "Рябко О.Г."
+        },
+        {
+            "name": "Гасанов Хабиб",
+            "gender": "М",
+            "age": "10",
+            "weight": "35",
+            "ku": "8",
+            "city": "Р. Дагестан",
+            "coach": "Темирханов М.Б."
+        },
+        {
+            "name": "Игнотенко Игорь",
+            "gender": "М",
+            "age": "11",
+            "weight": "35",
+            "ku": "7",
+            "city": "Михайловск",
+            "coach": "Якименко О.В."
+        },
+        {
+            "name": "Шегай Александр",
+            "gender": "М",
+            "age": "10",
+            "weight": "35",
+            "ku": "7",
+            "city": "Михайловск",
+            "coach": "Якименко О.В."
+        }
+    ]
+}
+
+let array = JSON.parse( JSON.stringify(json) );
+console.log(array);
+
+function getCurrenArray(data) {
+    let result;
+
+    for (let key in data) {
+        result = data[key];
+    }
+
+    for (let prop of result) {
+        for (let item in prop) {
+            if ( !isNaN(prop[item]) ) prop[item] = Number(prop[item]);
+        };
+    }
+
+    return result;
+};
+
+let currentArray = getCurrenArray(array);
+console.log(currentArray);
+
+function getPropertyes(data, property) {
+    let result = [];
+    result.push(data[0][property]);
+    data.forEach( (item, index, arr) => {
+        if (!result.includes(item[property])) result.push(item[property]);
+    })
+    return result.sort();
+}
+
+let coachesList =  getPropertyes( currentArray, 'coach' );
+console.log(coachesList);
+
+let sportsmenList = getPropertyes( currentArray, 'name');
+console.log(sportsmenList);
+
+function getCategories(data, weight, ageMin, ageMax, gender) {
+    let result = [];
+
+    for (let item of data) {
+        if (item.weight <= weight && item.gender == gender && (item.age == ageMin || item.age == ageMax) ) result.push(item);
+    }
+
+    return result;
+}
+
+let boys_10_11_under_30 = getCategories(currentArray, 30, 10, 11, 'М');
+console.log(boys_10_11_under_30);
+
+function shuffleCategories(categories) {
+    let result = [...categories];
+    for (let i = result.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [result[i], result[j]] = [result[j], result[i]];
+    }
+    return result;
+}
+
+let shuffleBoys_10_11_under_30 = shuffleCategories(boys_10_11_under_30);
+console.log(shuffleBoys_10_11_under_30);
+
+for (let i = 0; i < shuffleBoys_10_11_under_30.length; i += 2) {
+    console.log(`${shuffleBoys_10_11_under_30[i]} VS ${shuffleCategories[i+1]}`);
+}
